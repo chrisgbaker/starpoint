b87050cf4802cb47bffd4ef7a2ebfb58b40910db
cbaker <cbaker@ebscohost.com>
2016-10-07 16:59:26 -0400

    adding the beginning of webpack...holy dependencies..

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,6 @@
 module.exports = {
 	extends: 'standard',
 	rules: {
-		// Enforce anchor elements to have content
-		// https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-has-content.md
-		'jsx-a11y/anchor-has-content': [
-			2,
-			'AnchorLink'
-		],
-		// Enforce an anchor element's href prop value is not just #
-		// https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/href-no-hash.md
-		'jsx-a11y/href-no-hash': [
-			2,
-			'AnchorLink'
-		]
+		//'indent' : ['error', 'tab'],
 	}
 };
---
+++ webpack.debug.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const webpack = require('webpack')
+const packageJson = require('./package.json')
+const version = packageJson.version
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  entry: {
+    starpoint: [
+      'eventsource-polyfill', // necessary for hot reloading with IE
+      'webpack-hot-middleware/client',
+      'babel-polyfill', // Polyfill to create ES6 browser env
+      './src/index'
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, `dist/starpoint-${version}`)
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    root: path.resolve('./src'),
+    alias: {
+      'app.js': 'app.debug.js', // Includes devtools
+      'theme.scss': 'styles/theme.scss'
+    },
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
+    modulesDirectories: ['node_modules']
+  },
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }, {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['react-hmre']
+        },
+        include: [
+          path.resolve(__dirname, 'src')
+        ]
+      }, {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+      }, {
+        test: /\.(jpg)|(gif)|(png)$/,
+        loader: 'file'
+      }, {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ],
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: 'source-map-loader' }
+    ]
+  },
+  postcss: () =>
+    [autoprefixer({ browsers: ['last 2 versions'] })]
+}
