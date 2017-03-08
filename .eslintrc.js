b6a5699f274dff139fe23422ba14aeeb544b7ce8
chrisgbaker <christopher.baker1988@gmail.com>
2017-03-08 12:54:02 -0500

    updated eslint rules and added react-router types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
 	extends: 'standard',
 	rules: {
-		//'indent' : ['error', 'tab'],
+		// Enforce anchor elements to have content
+		// https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-has-content.md
+		'jsx-a11y/anchor-has-content': [
+			2,
+			'AnchorLink'
+		],
+		// Enforce an anchor element's href prop value is not just #
+		// https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/href-no-hash.md
+		'jsx-a11y/href-no-hash': [
+			2,
+			'AnchorLink'
+		],
 	}
 };
