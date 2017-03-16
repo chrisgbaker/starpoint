efe2cadfa91aa9f8bbce94ce86fc513fec7540b5
chrisgbaker <christopher.baker1988@gmail.com>
2017-03-16 15:36:52 -0400

    updating dependencies. react-router v4 was released but not updated. no @types available yet...

--- test/components/test-component/test-component-test.ts
+++ test/components/test-component/test-component-test.ts
@@ -4,6 +4,6 @@ import TestComponent from '../../../src/components/test-component'
 describe('First test', () => {
     it('should pass with flying colors', () => {
         const renderedApp = new TestComponent({message:'cool test!'}).render();
-        expect(renderedApp.props.children).to.equal('cool test');
+        expect(renderedApp.props.children).to.equal('cool test!');
     })
 })
